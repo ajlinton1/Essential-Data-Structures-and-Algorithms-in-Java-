15904a7bf754888ff7be0575ba5dfab6e22fbfe0
Andy Linton <ajlinton@hotmail.com>
2026-08-27 21:20:59 -0700

    Add DailyTemperatures class and test for Exercise 4

--- src/main/java/chapter3/DailyTemperatures.java
+++ src/main/java/chapter3/DailyTemperatures.java
@@ -2,7 +2,7 @@ package chapter3;
 
 public class DailyTemperatures {
 
-    public int[] dailyTemperatures(int[] temps) {
+/*    public int[] dailyTemperatures(int[] temps) {
         int[] answers = new int[temps.length];
         for (int i = 0; i < temps.length; i++) {
             for (int j = i + 1; j < temps.length; j++) {
@@ -13,5 +13,19 @@ public class DailyTemperatures {
             }
         }
         return answers;
+    } */
+
+    public int[] dailyTemperatures(int[] temps) {
+        int[] answers = new int[temps.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temps.length; i++) {
+            while (!stack.isEmpty() && temps[i] > temps[stack.peek()]) {
+                int index = stack.pop();
+                answers[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return answers;
     }
+
 }
